f6c58cef91ab1fe0e19d75ba8e5048e267053060
Jakob Odersky <jakob@odersky.com>
2016-05-20 14:54:41 -0700

    update drone

---
+++ .drone/authenv.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Set up the environment for signing and publishing sbt projects.
+# WARNING: this script has global side effects, it is intended to be
+# run in an isolated, throw-away environment!
+
+set -e
+
+# import public key from key servers
+gpg2 --batch --keyserver hkp://pool.sks-keyservers.net --recv-keys 4E7DA7B5A0F86992D6EB3F514601878662E33372
+
+# import secret signing sub key, the key is expected to be passwordless
+echo "$GPG_SSB" | gpg2 --batch --import
+
+# prepare gpg settings for sbt
+cat << EOF > gpg.sbt
+import com.typesafe.sbt.pgp.PgpKeys._
+pgpSigningKey in Global := Some(0x2CED17AB2B6D6F37l)
+pgpPassphrase in Global := None
+useGpg in Global := true
+EOF
+
+# prepare bintray settings
+mkdir -p "$HOME"/.bintray
+cat << EOF > "$HOME"/.bintray/.credentials
+realm = Bintray API Realm
+host = api.bintray.com
+user = jodersky
+password = "$BINTRAY_KEY"
+EOF
